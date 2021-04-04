66742e90f0f0466625a16e8f334dcb12485e3d24
Zach G <zag0003@auburn.edu>
2021-04-03 23:49:50 -0500

    included body-parser package and added it to express app with app.use

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //console.log('May Node be with you');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.listen(3000, function() {
     console.log('listening on port 3000');
@@ -12,5 +15,5 @@ app.get('/', (req, res) => {
 })
 
 app.post('/quotes', (req, res) => {
-    console.log('Helloooooooooooooooo!');
+    console.log(req.body);
 })
